3d7b0a445c4999861f16bd0e015eed9c0cb72f45
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-23 23:49:35 -0700

    removed listener when contained in iframe

--- arc-app/src/components/ui/Header.js
+++ arc-app/src/components/ui/Header.js
@@ -3,14 +3,11 @@ import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
 
 // adds subtle effect where header will lift when the page is scrolled
 function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
+  const { children } = props;
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
+    threshold: 0, // listener when to trigger the effect
   });
 
   return React.cloneElement(children, {
