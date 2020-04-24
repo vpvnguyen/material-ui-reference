6bf745d20fe2e63f6278befa0fb33b3e53b6d7f8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-23 23:15:26 -0700

    implement elevatedScrollBar

--- arc-app/src/components/App.js
+++ arc-app/src/components/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <Header />
+      {/* placeholder to show scrolling effect */}
+      {[...new Array(120)]
+        .map(
+          () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+        )
+        .join("\n")}
     </div>
   );
 }
--- arc-app/src/components/ui/Header.js
+++ arc-app/src/components/ui/Header.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+
+// adds subtle effect where header will lift when the page is scrolled
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 export default function Header(props) {
   return (
-    // has a default class of position='fixed'
-    <AppBar>
-      {/* allows content to scale horizontally across the Appbar */}
-      <Toolbar>Arc App</Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      {/* has a default class of position='fixed' */}
+      <AppBar>
+        {/* allows content to scale horizontally across the Appbar */}
+        <Toolbar>Arc App</Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 }
