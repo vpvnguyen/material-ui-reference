6ebd755a164c87aef43e618b2611d2db83723740
vpvnguyen <vpvnguyen@gmail.com>
2020-04-26 22:36:00 -0700

    removed placeholder with reminder text

--- arc-app/src/components/App.js
+++ arc-app/src/components/App.js
@@ -12,15 +12,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      {/* placeholder to show scrolling effect */}
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
+      Add content here
     </ThemeProvider>
   );
 }
