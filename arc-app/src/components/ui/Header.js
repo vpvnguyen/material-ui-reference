8b8c44c6e0a4975bba8a8388d6c6ce5df9a3d6c0
vpvnguyen <vpvnguyen@gmail.com>
2020-04-26 22:40:09 -0700

    push content below appbar

--- arc-app/src/components/ui/Header.js
+++ arc-app/src/components/ui/Header.js
@@ -5,6 +5,7 @@ import {
   Typography,
   useScrollTrigger,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 // adds subtle effect where header will lift when the page is scrolled
 function ElevationScroll(props) {
@@ -20,18 +21,30 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    // use spread operator to copy over properties from theme
+    ...theme.mixins.toolbar, // gives a bit of height to app bar to push content below the appbar
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      {/* has a default class of position='fixed' */}
-      <AppBar color="primary">
-        {/* allows content to scale horizontally across the Appbar */}
-        <Toolbar>
-          <Typography variant="h3" color="secondary">
-            Arc App
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        {/* has a default class of position='fixed' */}
+        <AppBar color="primary">
+          {/* allows content to scale horizontally across the Appbar */}
+          <Toolbar>
+            <Typography variant="h3" color="secondary">
+              Arc App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
