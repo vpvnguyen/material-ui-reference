2994b97d51a27ab828e4559eaca9314a6e6393a8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-04-28 23:41:35 -0700

    created routing with placeholder components

--- arc-app/src/components/App.js
+++ arc-app/src/components/App.js
@@ -1,18 +1,43 @@
 import React from "react";
-
-// components
-import Header from "../components/ui/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // material ui
 import { ThemeProvider } from "@material-ui/styles";
+
+// components
+import Header from "../components/ui/Header";
 
 import theme from "./ui/Theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Add content here
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>customsoftware</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>mobileapps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>about</div>} />
+          <Route exact path="/contact" component={() => <div>contact</div>} />
+          <Route exact path="/estimate" component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
